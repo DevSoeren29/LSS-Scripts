aeff5d1e536fa1a3789a37626d98bf6c34569e86
DevSoeren29 <93468567+DevSoeren29@users.noreply.github.com>
2023-01-07 22:43:35 +0100

    Create einsatzanzahl.user.js

---
+++ einsatzanzahl.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Einsatz Anzahl
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Zeigt an, wie viele Einsätze du insgesamt offen hast.
+// @author       Dev_Sören29#1385 aka. SJ_Luftpumpe
+// @match        https://www.leitstellenspiel.de/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=leitstellenspiel.de
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const foundtext = document.querySelector('.missions-panel-head.big_map_window_head strong')
+    //class1
+    const foundclass1 = document.querySelector('#mission_select_emergency')
+    const str1 = foundclass1.innerText
+    const str1sp = str1.split('/')
+    const str1re = str1sp[1].replace(')', ' ')
+    const str1pi = parseInt(str1re)
+    const str1main = str1pi
+    //class2
+    const foundclass2 = document.querySelector('#mission_select_krankentransport')
+    const str2 = foundclass2.innerText
+    const str2sp = str2.split('/')
+    const str2re = str2sp[1].replace(')', ' ')
+    const str2pi = parseInt(str2re)
+    const str2main = str2pi
+    //class3
+    const foundclass3 = document.querySelector('#mission_select_sicherheitswache')
+    const str3 = foundclass3.innerText
+    const str3sp = str3.split('/')
+    const str3re = str3sp[1].replace(')', ' ')
+    const str3pi = parseInt(str3re)
+    const str3main = str3pi
+
+    //main
+    foundtext.innerText = 'Einsätze: '+(str1main+str2main+str3main)
+})();
