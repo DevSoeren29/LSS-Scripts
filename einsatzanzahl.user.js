8f730248fee6ffbea07286a57a75462334c362ac
DevSoeren29 <93468567+DevSoeren29@users.noreply.github.com>
2023-01-08 12:43:50 +0100

    No comment

--- einsatzanzahl.user.js
+++ einsatzanzahl.user.js
@@ -46,44 +46,61 @@
 // @grant        unsafeWindow
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     const missionMakerAddOrig = unsafeWindow.missionMakerAdd;
-    const foundtext = document.querySelector('.missions-panel-head.big_map_window_head strong');
-    
-    const updateEinsatzzahl = () => {
-       //class1
-       const foundclass1 = document.querySelector('#mission_select_emergency')
-       const str1 = foundclass1.innerText
-       const str1sp = str1.split('/')
-       const str1re = str1sp[1].replace(')', ' ')
-       const str1pi = parseInt(str1re)
-       const str1main = str1pi
-    
-       //class2
-      const foundclass2 = document.querySelector('#mission_select_krankentransport')
-      const str2 = foundclass2.innerText
-      const str2sp = str2.split('/')
-      const str2re = str2sp[1].replace(')', ' ')
-      const str2pi = parseInt(str2re)
-      const str2main = str2pi
-    
-      //class3
-      const foundclass3 = document.querySelector('#mission_select_sicherheitswache')
-      const str3 = foundclass3.innerText
-      const str3sp = str3.split('/')
-      const str3re = str3sp[1].replace(')', ' ')
-      const str3pi = parseInt(str3re)
-      const str3main = str3pi
+    const foundtext = document.querySelector(
+        '.missions-panel-head.big_map_window_head strong'
+    );
 
-      //main
-      console.log(str1main+str2main+str3main)
-      foundtext.innerText = foundtext.innerText+': '+(str1main+str2main+str3main) 
+    if (!foundtext) return;
+
+    const updateEinsatzzahl = () => {
+        //class1
+        const foundclass1 = document.querySelector('#mission_select_emergency');
+        let str1main = 0;
+        if (foundclass1) {
+            const str1 = foundclass1.innerText;
+            const str1sp = str1.split('/');
+            const str1re = str1sp[1].replace(')', ' ');
+            const str1pi = parseInt(str1re);
+            str1main = str1pi;
+        }
+
+        //class2
+        const foundclass2 = document.querySelector(
+            '#mission_select_krankentransport'
+        );
+        let str2main = 0;
+        if (foundclass2) {
+            const str2 = foundclass2.innerText;
+            const str2sp = str2.split('/');
+            const str2re = str2sp[1].replace(')', ' ');
+            const str2pi = parseInt(str2re);
+            str2main = str2pi;
+        }
+
+        //class3
+        const foundclass3 = document.querySelector(
+            '#mission_select_sicherheitswache'
+        );
+        let str3main = 0;
+        if (foundclass3) {
+            const str3 = foundclass3.innerText;
+            const str3sp = str3.split('/');
+            const str3re = str3sp[1].replace(')', ' ');
+            const str3pi = parseInt(str3re);
+            str3main = str3pi;
+        }
+
+        //main
+        console.log(str1main + str2main + str3main);
+        foundtext.innerText += `: ${str1main + str2main + str3main}`;
     };
 
     unsafeWindow.missionMakerAdd = function (...args) {
         missionMakerAddOrig(...args);
-        
+
         updateEinsatzzahl();
     };
     updateEinsatzzahl();
